b289faec8615ba2842ab46ea2769f5b3ca2e2541
aldo2502 <151655813+aldo2502@users.noreply.github.com>
2025-06-07 15:51:51 -0300

    Update CartasSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -3,7 +3,6 @@ main() {
     //dados da primeira carta
     int populacao1, pontos_turisticos1;
     float area1, pib1;
-    //dados da segunda carta
     int populacao2, pontos_turisticos2;
     float area2, pib2;
     
